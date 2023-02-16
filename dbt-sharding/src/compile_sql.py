16f95d45423a327861005fdd1d59324a365f7328
afinkelstein <afinkelstein@salesforce.com>
2023-02-16 12:50:40 +0200

    create sharding table sql

--- dbt-sharding/src/compile_sql.py
+++ dbt-sharding/src/compile_sql.py
@@ -3,22 +3,70 @@ import hashlib
 import os
 import requests
 import json
+import queue
 
 import model_utils
 import sharding_client
+import threading
 
-SERVER_HOST_PUSH = "http://0.0.0.0:8585/push"
-SERVER_HOST_PARSE = "http://0.0.0.0:8585/parse"
-SERVER_HOST_COMPILE = "http://0.0.0.0:8585/compile"
-SERVER_HOST_MEM = "http://0.0.0.0:8585/"
+SERVER_HOST_PUSH = "http://0.0.0.0:%d/push"
+SERVER_HOST_PARSE = "http://0.0.0.0:%d/parse"
+SERVER_HOST_COMPILE = "http://0.0.0.0:%d/compile"
 
 project_dir = sys.argv[1]
+model_version = int(sys.argv[2])
+
+prim_shard = int(sys.argv[3])
+sec_shard = int(sys.argv[4])
 
 model_id = hashlib.md5(os.path.abspath(project_dir).encode()).hexdigest()
 
-print("model_id =", model_id)
 
-print(sharding_client.get_or_assign_shards(model_id, 1, 2))
+def update_shard(shard_id, is_primary, q):
+    shard_port = 8580 + shard_id
+
+    state = model_utils.get_state(project_dir)
+
+    req = requests.post(SERVER_HOST_PUSH % shard_port, json={"state_id": model_id, "body": state})
+    if req.status_code != 200:
+        return
+    # # #
+    # # # # POST /parse
+    req = requests.post(SERVER_HOST_PARSE % shard_port, json={"state_id": model_id})
+    if req.status_code != 200:
+        print("could not parse shard",shard_id)
+        return
+
+    sharding_client.update_shard_version(model_id, model_version, is_primary)
+
+    q.put(shard_id)
+
+
+print("model_id = %s, version = %d" % (model_id, model_version))
+
+shard1, ver1, shard2, ver2 = sharding_client.get_or_assign_shards(model_id, prim_shard, sec_shard)
+
+print("identified shards and versions", shard1, ver1, shard2, ver2)
+
+shard1_good = ver1 and ver1 == model_version
+shard2_good = ver2 and ver2 == model_version
+
+good_shards_q = queue.Queue()
+
+if shard1_good:
+    good_shards_q.put(shard1)
+else:
+    threading.Thread(target = update_shard, args = (shard1, True, good_shards_q)).start()
+if shard2_good:
+    good_shards_q.put()
+else:
+    threading.Thread(target = update_shard, args = (shard2, False, good_shards_q)).start()
+
+print("waiting for a ready shard...")
+good_shard_id = good_shards_q.get(block=True, timeout=60)
+print("shard %d is ready, compiling" % good_shard_id)
+
+shard_port = good_shard_id + 8580
 
 sql = """
 select *
@@ -32,14 +80,5 @@ from {{ metrics.calculate(
     }}
 """
 
-state = model_utils.get_state(project_dir)
-
-req = requests.post(SERVER_HOST_PUSH, json={"state_id": model_id, "body": state})
-print("PUSH", req.status_code)
-# # #
-# # # # POST /parse
-req = requests.post(SERVER_HOST_PARSE, json={"state_id": model_id})
-print("PARSE", req.status_code)
-
-resp = requests.post(SERVER_HOST_COMPILE, json={"state_id": model_id, "sql": sql, "target": "dev"})
+resp = requests.post(SERVER_HOST_COMPILE % shard_port, json={"state_id": model_id, "sql": sql, "target": "dev"})
 print("sql: ", json.loads(resp.content)['res']['compiled_code'])
--- dbt-sharding/src/sharding_client.py
+++ dbt-sharding/src/sharding_client.py
@@ -18,3 +18,10 @@ def get_or_assign_shards(model_id, prim_id, second_id):
         c.execute("insert into sharding(model_id,primary_shard_id,secondary_shard_id) values(%s,%s,%s)", (model_id, prim_id, second_id))
         conn.commit()
         return [prim_id, None, second_id, None]
+
+
+def update_shard_version(model_id, model_version, is_primary):
+    with conn.cursor() as c:
+        shard_col = "primary_version" if is_primary else "secondary_version"
+        c.execute("update sharding set "+shard_col+" = %s where model_id = %s ", (model_version, model_id))
+        conn.commit()
